0efe6e869108267eca48a947a27ffe23cbff49b9
Martin Todorov <carlspring@gmail.com>
2023-11-30 20:06:38 +0200

    * Configure Plugin and Project Dependency Resolution Management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "Contrast-Sample-Gradle-Application"
+
+// Plugin Dependency Resolution Management
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+// Project Dependency Resolution Management
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
